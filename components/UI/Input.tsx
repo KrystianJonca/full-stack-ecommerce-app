fd7a55942d15d93980528a4bba160521f28627b5
Wionek <krystianjonca43@gmail.com>
2022-05-07 13:17:10 +0200

    Added input

---
+++ components/UI/Input.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+interface IInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  name: string;
+  type: string;
+}
+
+const Input: React.FC<IInputProps> = ({ label, name, type, ...rest }) => {
+  return (
+    <div>
+      <label htmlFor={name} className="block py-2 text-lg">
+        {label}
+      </label>
+      <input
+        {...rest}
+        type={type}
+        name={name}
+        placeholder=" "
+        className="w-full bg-transparent outline-none border-2 focus:border-primary-light border-gray-400 hover:border-primary-light py-2 px-3 transition-all"
+      />
+    </div>
+  );
+};
+
+export default Input;
