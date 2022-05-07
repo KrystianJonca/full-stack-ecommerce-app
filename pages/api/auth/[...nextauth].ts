53258f30179de18d20853ba4e6a5964cb61c88d2
Wionek <krystianjonca43@gmail.com>
2022-05-07 22:52:55 +0200

    Added passwordless auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,16 @@
+import NextAuth from 'next-auth/next';
+import EmailProvider from 'next-auth/providers/email';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { prisma } from '../../../utils/db';
+import getEnv from '../../../utils/env';
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: getEnv('EMAIL_SERVER'),
+      from: getEnv('EMAIL_FROM'),
+    }),
+  ],
+  secret: getEnv('SECRET'),
+});
