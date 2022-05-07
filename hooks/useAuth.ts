58660c89a33241d70628349a0914b5bfbc700a2b
Wionek <krystianjonca43@gmail.com>
2022-05-07 22:53:27 +0200

    Added helper useAuth hook

---
+++ hooks/useAuth.ts
@@ -0,0 +1,19 @@
+import { useMemo } from 'react';
+import { useSession, signIn, signOut } from 'next-auth/react';
+
+const useAuth = () => {
+  const { data: session, status } = useSession();
+
+  return useMemo(
+    () =>
+      ({
+        session,
+        status,
+        signIn,
+        signOut,
+      } as const),
+    [session, status],
+  );
+};
+
+export default useAuth;
