144169b7a93069c4861686ada8e446056f2e75bf
Wionek <krystianjonca43@gmail.com>
2022-05-08 10:45:59 +0200

    Changed styles

--- components/UI/Input.tsx
+++ components/UI/Input.tsx
@@ -4,12 +4,13 @@ interface IInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
   name: string;
   type: string;
+  dark: boolean;
 }
 
-const Input: React.FC<IInputProps> = ({ label, name, type, ...rest }) => {
+const Input: React.FC<IInputProps> = ({ label, name, type, dark, ...rest }) => {
   return (
     <div>
-      <label htmlFor={name} className="block py-2 text-lg">
+      <label htmlFor={name} className="block py-2 text-xl">
         {label}
       </label>
       <input
@@ -17,7 +18,11 @@ const Input: React.FC<IInputProps> = ({ label, name, type, ...rest }) => {
         type={type}
         name={name}
         placeholder=" "
-        className="w-full bg-transparent outline-none border-2 focus:border-primary-light border-gray-200 hover:border-primary-light py-2 px-3 transition-all"
+        className={`w-full bg-transparent outline-none border-2  py-2 px-3 transition-all ${
+          !dark
+            ? 'focus:border-primary-light border-gray-200 hover:border-primary-light'
+            : 'focus:border-primary-dark border-gray-800 hover:border-primary-dark'
+        } `}
       />
     </div>
   );
