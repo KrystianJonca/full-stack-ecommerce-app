21d8ad145bd2a5bb22cb9bda5676225952e1a390
Wionek <krystianjonca43@gmail.com>
2022-05-09 20:01:33 +0200

    Added react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import '../styles/tailwind.css';
 import { SessionProvider } from 'next-auth/react';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </SessionProvider>
   );
 }
