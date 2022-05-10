bc70ed2b83b6d057e033c0819e6c63179c923cbd
Wionek <krystianjonca43@gmail.com>
2022-05-10 12:02:41 +0200

    Changed the export

--- hooks/useCart.ts
+++ hooks/useCart.ts
@@ -1,10 +1,12 @@
 import { useContext } from 'react';
 import { CartContext } from '../context/cartContext';
 
-export const useCart = () => {
+const useCart = () => {
   const context = useContext(CartContext);
   if (context === undefined)
     throw new Error('useCart hook must be used withing cart context provider');
 
   return context;
 };
+
+export default useCart;
